0925f2d1576bb4d9f587f6bf8e7d427225a4c279
ZzzwaHoon <kkjw94@gmail.com>
2024-04-01 18:04:29 +0900

    javaScript study - operator

---
+++ operator.js
@@ -0,0 +1,8 @@
+console.log(1 + 2);           // 더하기
+console.log(2 * 5);           // 곱하기
+console.log(1 / 3);           // 나누기
+console.log(1 / 2);
+
+console.log(0.1 + 0.2);       // 프로그램언어의 오류 (2진수로 바꾸면서 발생하는)
+
+console.log(11 % 2);          // 나눈 뒤 나머지
